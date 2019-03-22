3b64e9d9607bb202009b79991aa08e051c4c4062
Lexa <lexa2987@mail.ru>
2019-03-22 22:55:38 +0300

    first screen

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './css/bootstrap/bootstrap-grid.min.css';
 import './css/bootstrap/bootstrap-reboot.min.css';
 import './css/bootstrap/bootstrap.min.css';
 import './js/index';
-import './css/index.css';
+import './css/startScreen/index.css';
