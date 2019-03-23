b7c9ad62cd0725b344fd25d7578bd5241615e5c1
Lexa <lexa2987@mail.ru>
2019-03-23 12:43:02 +0300

    tutorial

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import './css/bootstrap/bootstrap-reboot.min.css';
 import './css/bootstrap/bootstrap.min.css';
 import './js/index';
 import './css/startScreen/index.css';
+import './css/tutorial/index.css';
+
+
+const toggles = document.querySelectorAll('.c-hamburger');
+const toggleHandler = toggle => {
+    toggle.addEventListener('click', function (e) {
+        e.preventDefault();
+        this.classList.toggle('is-active');
+    });
+};
+toggles.forEach(item => toggleHandler(item));
